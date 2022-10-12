3290f4906af7706f943948e909d87b7106d8568a
hoonkim <hoonk9387@gmail.com>
2022-10-12 11:26:45 -0500

    Add comment to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  # GET  /reviews(.:format)        reviews#index
+  # GET  /dog_houses/:id(.:format) dog_houses#show
   resources :reviews, only: [:index]
   resources :dog_houses, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
